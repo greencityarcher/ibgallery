70388f554835408202ac276b9b1048f7e315bc0b
Unknown <pingttl128@gmail.com>
2017-06-14 17:30:24 +0300

    добавлены заголовки для всех уровней подменю  условие, по которому сворачивание мобильного меню работает только на узком экране

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -5,6 +5,7 @@ var menuOpened = false;
 $("#mobile-menu-btn").click(function() {
   if ((menuOpened) && ($(window).width()<980)) {
     $("#main-nav").removeClass("opened");
+    $("#mobile-menu-btn").removeClass("main-nav__mobile-menu-btn--close");
     $("#sidebar").show();
     $("#content").show();
     $('#footer-social-links').show();
@@ -12,6 +13,7 @@ $("#mobile-menu-btn").click(function() {
   } else {
 
     $("#main-nav").addClass("opened");
+    $("#mobile-menu-btn").addClass("main-nav__mobile-menu-btn--close");
     $("#sidebar").hide();
     $('#footer-social-links').hide();
     $("#content").hide();
@@ -22,29 +24,24 @@ $("#mobile-menu-btn").click(function() {
 
 //открывание разделов подменю
 
-if ($(window).width()<980){
+
 
 $(".mobile-menu-link").click(function() {
+  if ($(window).width()<980){
     $(this).hide();
     $(this).siblings().show();
     $(this).parent().siblings().hide();
+}
 
 });
-}
-else{
-
-}
 
 //возврат по кнопке "назад"
 
-if ($(window).width()<980){
+
 $(".sub-menu__link--back").click(function(){
+  if ($(window).width()<980){
   $(this).parent().parent().hide(); //скрываем подменю
   $(this).parent().parent().parent().children().first().show(); //показываем ссылку на родительский раздел
   $(this).parent().parent().parent().siblings().show();//и остальные разделы этого уровня
-
+}
 });
-}
-else{
-
-}
